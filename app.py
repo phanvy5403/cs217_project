70ec81b3989bc0bae685552b68314c1ac73cc877
Vy Phan Quoc <vy.phan@bizone.com.vn>
2025-01-07 22:55:03 +0700

    update ui

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 import os
 from functools import wraps
 import logging
-from logging.handlers import RotatingFileHandler  # Add this import
+from logging.handlers import RotatingFileHandler  
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
